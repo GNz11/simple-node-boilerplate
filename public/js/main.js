75b0203277ea186bc3e084cec466efeda3c89a43
GNz11 <sshammasova@gmail.com>
2020-02-12 19:22:55 -0500

    updated

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,9 @@
+import axios from 'axios';
+
 // jest won't error out on DOM calls.
-console.log(document.querySelector('p').innerHTML);
+// console.log(document.querySelector('p').innerHTML);
+console.log("hellooooo");
+
+export const getReservations = async () => {
+  const { data } = await axios.get('http://localhost:3000/reservations');
+};
